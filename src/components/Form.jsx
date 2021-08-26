16d55ad324fc0554968018f8fb9165e7d8e664e9
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-08-26 21:57:43 +0100

    Create event handler functions; keep changes in state.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,66 @@
 import { useState } from "react";
+// TODO: create submitHandler()
 
 function Form() {
+  // state
+  const [color, setColor] = useState(null);
+  const [spendTime, setSpendTime] = useState([]);
+  const [review, setReview] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  console.log("State: ", {
+    color,
+    spendTime,
+    review,
+    username,
+    email,
+  });
+
+  /* HANDLER FUNCTIONS */
+  //   radio input handler
+
+  const handleRadioInput = (event) => {
+    console.log("inside handleRadioInput: ", event.target.value);
+    setColor(event.target.value);
+  };
+
+  //   checkbox input handler
+
+  const handleSpendTimeInput = (event) => {
+    console.log(
+      "Inside handleSpendTimeInput: ",
+      event.target.value,
+      event.target.checked
+    );
+
+    if (event.target.checked) {
+      setSpendTime([...spendTime, event.target.value]);
+    } else {
+      const filteredSpendTime = spendTime.filter(
+        (time) => event.target.value !== time
+      );
+      setSpendTime(filteredSpendTime);
+    }
+  };
+
+  //   text input handlers
+  const handleTexareaInput = (event) => {
+    console.log("Inside handleTexareaInput: ", event.target.value);
+    setReview(event.target.value);
+  };
+
+  const handleUsernameInput = (event) => {
+    console.log("Inside handleUsernameInput: ", event.target.value);
+    setUsername(event.target.value);
+  };
+
+  const handleEmailInput = (event) => {
+    console.log("Inside handleEmailInput: ", event.target.value);
+    setEmail(event.target.value);
+  };
+
+  // Component
   return (
     <form className="form">
       <h2>Tell us what you think about your rubber duck!</h2>
@@ -13,6 +73,7 @@ function Form() {
               type="radio"
               name="color"
               value="1"
+              onChange={handleRadioInput}
             />
             <label htmlFor="color-one">1</label>
           </li>
@@ -22,6 +83,7 @@ function Form() {
               type="radio"
               name="color"
               value="2"
+              onChange={handleRadioInput}
             />
             <label htmlFor="color-two">2</label>
           </li>
@@ -31,6 +93,7 @@ function Form() {
               type="radio"
               name="color"
               value="3"
+              onChange={handleRadioInput}
             />
             <label htmlFor="color-three">3</label>
           </li>
@@ -40,6 +103,7 @@ function Form() {
               type="radio"
               name="color"
               value="4"
+              onChange={handleRadioInput}
             />
             <label htmlFor="color-four">4</label>
           </li>
@@ -50,26 +114,47 @@ function Form() {
         <ul>
           <li>
             <label>
-              <input name="spend-time" type="checkbox" value="swimming" />
+              <input
+                name="spend-time"
+                type="checkbox"
+                value="swimming"
+                // checked={spendTime.includes("swimming")}
+                onChange={handleSpendTimeInput}
+              />
               Swimming
             </label>
           </li>
           <li>
             <label>
-              <input name="spend-time" type="checkbox" value="bathing" />
+              <input
+                name="spend-time"
+                type="checkbox"
+                value="bathing"
+                onChange={handleSpendTimeInput}
+              />
               Bathing
             </label>
           </li>
           <li>
             <label>
-              <input name="spend-time" type="checkbox" value="chatting" />
+              <input
+                name="spend-time"
+                type="checkbox"
+                value="chatting"
+                onChange={handleSpendTimeInput}
+              />
               Chatting
             </label>
           </li>
           <li>
             <label>
-              <input name="spend-time" type="checkbox" value="noTime" />I don't
-              like to spend time with it
+              <input
+                name="spend-time"
+                type="checkbox"
+                value="noTime"
+                onChange={handleSpendTimeInput}
+              />
+              I don't like to spend time with it
             </label>
           </li>
         </ul>
@@ -80,6 +165,8 @@ function Form() {
           name="review"
           cols="30"
           rows="10"
+          value={review}
+          onChange={handleTexareaInput}
         ></textarea>
       </label>
       <label>
@@ -87,7 +174,8 @@ function Form() {
         <input
           type="text"
           name="username"
-          value=""
+          value={username}
+          onChange={handleUsernameInput}
         />
       </label>
       <label>
@@ -95,7 +183,8 @@ function Form() {
         <input
           type="email"
           name="email"
-          value=""
+          value={email}
+          onChange={handleEmailInput}
         />
       </label>
       <input className="form__submit" type="submit" value="Submit Survey!" />
